1978889aa9cfc88c604d8437bcd240a6330e4e91
GotouTamaki <134473633+GotouTamaki@users.noreply.github.com>
2023-08-27 18:33:21 +0900

    [update]アイテムの機能の更新

--- Assets/tamaki/Scripts/ItemBase.cs
+++ Assets/tamaki/Scripts/ItemBase.cs
@@ -4,12 +4,19 @@ public class ItemBase : MonoBehaviour
 {
     [SerializeField] Item _item = null;
 
+    private void Start()
+    {
+        _item = GetComponent<Item>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player" && _item.ItemType == Item.ItemTypeEnum.HealItem)
         {
             //Playerのステータスを入手する
             //その後ItemGetの数値で変化をさせる
+            PlayerController playerController = collision.GetComponent<PlayerController>();
+            PlayerController.Instance.PlayerHeal(_item.ItemGet);
         }
         else if (collision.tag == "Player" && _item.ItemType == Item.ItemTypeEnum.WeaponItem)
         {
