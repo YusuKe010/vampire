792ed08f3a764fffc66291a5422ac001b7d4866b
GotouTamaki <134473633+GotouTamaki@users.noreply.github.com>
2023-08-27 13:26:36 +0900

    [add]アイテムの作成
    [add]自分用のファイルの作成

---
+++ Assets/tamaki/Scripts/Item.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+[CreateAssetMenu(fileName = "Item", menuName = "CreateItem")]
+public class Item : ScriptableObject
+{
+    [SerializeField] string _itemName = string.Empty;
+    public string ItemName => _itemName;
+    [SerializeField] ItemTypeEnum _itemType = ItemTypeEnum.HealItem;
+    public ItemTypeEnum ItemType => _itemType;
+    [SerializeField] int _itemGet = 1;
+    public int ItemGet => _itemGet;
+
+    public enum ItemTypeEnum
+    {
+        HealItem,
+        WaponItem,
+    }
+}
---
+++ Assets/tamaki/Scripts/ItemBase.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class ItemBase : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Playerのステータスを入手する
+    }
+}
